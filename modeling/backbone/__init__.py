91017313e366665274dc9cd7822c2f0a7debe9be
omers <omershect@users.noreply.github.com>
2019-09-26 10:31:50 +0300

    Update __init__.py
    Add Pretrained control optin for Xception

--- modeling/backbone/__init__.py
+++ modeling/backbone/__init__.py
@@ -1,10 +1,10 @@
 from modeling.backbone import resnet, xception, drn, mobilenet
 
-def build_backbone(backbone, output_stride, BatchNorm):
+def build_backbone(backbone, output_stride, BatchNorm,pretrained=True):
     if backbone == 'resnet':
         return resnet.ResNet101(output_stride, BatchNorm)
     elif backbone == 'xception':
-        return xception.AlignedXception(output_stride, BatchNorm)
+        return xception.AlignedXception(output_stride, BatchNorm,pretrained)
     elif backbone == 'drn':
         return drn.drn_d_54(BatchNorm)
     elif backbone == 'mobilenet':
